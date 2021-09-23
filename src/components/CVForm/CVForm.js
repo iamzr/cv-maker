9c8a900b3806abc241d6500213a6e7d4861023b1
Zia Rehman <iamziarehman@gmail.com>
2021-09-23 21:43:21 +0100

    Adding personal section

--- src/components/CVForm/CVForm.js
+++ src/components/CVForm/CVForm.js
@@ -1,7 +1,28 @@
+import { enumDeclaration } from "@babel/types";
 import React, { Component } from "react";
 
 class CVForm extends Component {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <h2>Personal</h2>
+          <label htmlfor="first-name">First name</label>
+          <input id="first-name"></input>
+          <label htmlfor="last-name">Last Name</label>
+          <input id="last-name"></input>
+          <label htmlfor="phone">Phone Number</label>
+          <input id="phone" type="number"></input>
+          <label htmlfor="address">Address</label>
+          <input id="address"></input>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default CVForm;
