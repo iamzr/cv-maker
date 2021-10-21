c551f568cadbd15f92e7815d200a5a63288f5a83
Zia Rehman <iamziarehman@gmail.com>
2021-10-21 20:42:34 +0100

    Refactored into functional component

--- src/components/CVForm/personal.js
+++ src/components/CVForm/personal.js
@@ -1,21 +1,14 @@
-import { Component } from "react";
 import PersonalItem from "./personalItem";
 
-class Personal extends Component {
-  constructor(props) {
-    super(props);
-  }
+function Personal(props) {
+  const { updateState } = props;
 
-  render() {
-    const { updateState } = this.props;
-
-    return (
-      <div id="personal">
-        <h2>Personal</h2>
-        <PersonalItem updateState={updateState}></PersonalItem>
-      </div>
-    );
-  }
+  return (
+    <div id="personal">
+      <h2>Personal</h2>
+      <PersonalItem updateState={updateState}></PersonalItem>
+    </div>
+  );
 }
 
 export default Personal;
