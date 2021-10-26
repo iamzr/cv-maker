b2c6ead99dcd7894c6dbd7ee67ba4a642182dea7
Zia Rehman <iamziarehman@gmail.com>
2021-10-27 00:58:49 +0100

    Rewrote the app, all in App.js. Removed strictmode to prevent double rendering

--- src/App.js
+++ src/App.js
@@ -1,56 +1,228 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import CVForm from "./components/CVForm/CVForm";
-import CVPreview from "./components/CVPreview/CVPreview";
+import { v4 as uuid } from "uuid";
+
+import Section from "./section";
 
 function App() {
-  let [state, setState] = useState({
-    personal: {},
-    experience: {},
-    education: {},
-    skills: {},
+  const personalItem = {
+    id: uuid(),
+    item: {
+      name: { id: "name", name: "Name", type: "text", value: "" },
+      phone: {
+        id: "phone",
+        name: "Phone Number",
+        type: "number",
+        value: "",
+      },
+      email: { id: "email", name: "Email", type: "email", value: "" },
+      address: { id: "address", name: "Address", type: "text", value: "" },
+    },
+  };
+
+  const experienceItem = {
+    id: uuid(),
+    item: {
+      title: { id: "title", name: "Title", type: "text", value: "" },
+      company: { id: "company", name: "Company", type: "text", value: "" },
+      startDate: {
+        id: "startDate",
+        name: "Start Date",
+        type: "date",
+        value: "",
+      },
+      endDate: { id: "endDate", name: "End Date", type: "date", value: "" },
+      text: { id: "text", name: "Details", type: "text", value: "" },
+    },
+  };
+
+  const educationItem = {
+    id: uuid(),
+    item: {
+      institute: {
+        id: "institute",
+        name: "Institute",
+        type: "text",
+        value: "",
+      },
+      qual: { id: "qual", name: "Qualification", type: "text", value: "" },
+      grade: { id: "grade", name: "grade", type: "text", value: "" },
+      startDate: {
+        id: "startDate",
+        name: "Start Date",
+        type: "date",
+        value: "",
+      },
+      endDate: { id: "endDate", name: "End Date", type: "date", value: "" },
+    },
+  };
+
+  const skillItem = {
+    id: uuid(),
+    item: {
+      skill: { id: "skill", name: "Skill", type: "text", value: "" },
+    },
+  };
+
+  const [cv, setCV] = useState({
+    personal: [],
+    experience: [],
+    education: [],
+    skills: [],
   });
 
-  function updateState(e) {
-    const parentNode = e.target.parentNode.parentNode.id;
-    console.log(parentNode);
-    const id = e.target.parentNode.id;
-    setState((prevState) => ({
-      ...prevState,
-      [parentNode]: {
-        ...prevState[parentNode],
-        [id]: {
-          ...prevState[parentNode][id],
-          [e.target.name]: e.target.value,
-        },
-      },
-    }));
-  }
-
-  function removeItem(e) {
-    const parentNode = e.target.parentNode.parentNode.id;
-    const id = e.target.parentNode.id;
-    setState((state) => {
-      state[parentNode][id] = undefined;
-      return state;
+  function addItem(item, section) {
+    setCV((cv) => {
+      console.log(["section", section]);
+      cv[section] = [...cv[section], item];
+      return { ...cv };
     });
   }
 
-  useEffect(() => {
-    console.log(state.experience);
-  }, [state]);
+  function removeItem(section, index) {
+    setCV((cv) => {
+      cv[section].splice(index, 1);
+      return { ...cv };
+    });
+  }
 
+  function handleChange(e, section, itemIndex, id) {
+    console.log(e.target.value);
+    setCV((cv) => {
+      cv[section][itemIndex]["item"][id]["value"] = e.target.value;
+      return { ...cv };
+    });
+  }
+
+  console.log(cv);
   return (
-    <div className="App">
-      <Header></Header>
-      <div className="container">
-        <CVForm
-          state={state}
-          updateState={updateState}
-          removeItem={removeItem}
-        ></CVForm>
-        {/* <CVPreview data={state}></CVPreview> */}
+    <div>
+      {/* <div className="cv-form">
+        {cv.personal.map((i, index) => {
+          const item = i.item;
+          const id = i.id;
+          const section = "personal";
+          return (
+            <div key={id} id={id}>
+              <React.Fragment>
+                {renderItem(item, index, section)}
+              </React.Fragment>
+              <button
+                onClick={() => {
+                  console.log("removed");
+                  removeItem("personal", index);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+        <button
+          onClick={() => {
+            console.log("clicked");
+            addItem(personalItem, "personal");
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <div> */}
+      <Section
+        cv={cv}
+        section="personal"
+        sectionItem={personalItem}
+        addItem={addItem}
+        removeItem={removeItem}
+        handleChange={handleChange}
+      ></Section>
+      <Section
+        cv={cv}
+        section="experience"
+        sectionItem={experienceItem}
+        addItem={addItem}
+        removeItem={removeItem}
+        handleChange={handleChange}
+      ></Section>
+      <Section
+        cv={cv}
+        section="education"
+        sectionItem={educationItem}
+        addItem={addItem}
+        removeItem={removeItem}
+        handleChange={handleChange}
+      ></Section>
+      <Section
+        cv={cv}
+        section="skills"
+        sectionItem={skillItem}
+        addItem={addItem}
+        removeItem={removeItem}
+        handleChange={handleChange}
+      ></Section>
+      <div>
+        <h2>Preview</h2>
+        <div>
+          {cv.personal.map((e) => {
+            const element = e.item;
+            return (
+              <div className="header">
+                <div className="name">{element.name.value}</div>
+                <div>
+                  {element.phone.value} | {element.email.value}
+                </div>
+                <div>{element.address.value}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {cv.experience.map((e) => {
+            const experience = e.item;
+            return (
+              <div>
+                <div className="line1">
+                  <div>{experience.title.value}</div>
+                  <div className="dates">
+                    {experience.startDate.value} - {experience.endDate.value}
+                  </div>
+                </div>
+                <div className="line2">
+                  <div>{experience.company.value}</div>
+                </div>
+                <div className="line3">
+                  <div>{experience.text.value}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        {cv.education.map((e) => {
+          const education = e.item;
+          return (
+            <div key={e.id}>
+              <div className="line1">
+                <div>{education.institute.value}</div>
+                <div className="dates">
+                  {education.startDate.value} - {education.endDate.value}
+                </div>
+              </div>
+              <div className="line3">
+                <div>{education.qual.value}</div>
+                <div>{education.grade.value}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {cv.skills.map((e) => {
+          const skill = e.item;
+          return <li>{skill.skill.value}</li>;
+        })}
       </div>
     </div>
   );
--- src/section.js
+++ src/section.js
@@ -1,9 +1,68 @@
-import data from "./src/data";
+import React from "react";
 
 function Section(props) {
-  const { sectionName } = props;
+  const { cv, section, sectionItem, addItem, removeItem, handleChange } = props;
 
-  return <div>{console.log(data.cv)}</div>;
+  function renderItem(item, itemIndex, section) {
+    let divs = [];
+    // item.forEach((element, idx) => {
+
+    for (let element in item) {
+      const id = item[element]["id"];
+      divs.push(
+        <React.Fragment>
+          <label
+            key={"label-" + itemIndex + id}
+            id={"label-" + itemIndex + id}
+            htmlFor={id}
+          >
+            {item[element]["name"]}
+          </label>
+          <input
+            key={"input-" + itemIndex + id}
+            id={"input-" + itemIndex + id}
+            name={id}
+            type={item[element]["type"]}
+            defaultValue={item[element]["value"]}
+            onChange={(e) => handleChange(e, section, itemIndex, id)}
+          ></input>
+        </React.Fragment>
+      );
+    }
+    console.log("renderItem done");
+    return divs;
+  }
+
+  return (
+    <div>
+      <h2>{section}</h2>
+      {cv[section].map((i, index) => {
+        const item = i.item;
+        const id = i.id;
+        return (
+          <div key={id} id={id}>
+            <React.Fragment>{renderItem(item, index, section)}</React.Fragment>
+            <button
+              onClick={() => {
+                console.log("removed");
+                removeItem(section, index);
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        onClick={() => {
+          console.log("clicked");
+          addItem(sectionItem, section);
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default Section;
