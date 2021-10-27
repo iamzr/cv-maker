d418e0a585fdd4c709e1241d1585bdef6b66c718
Zia Rehman <iamziarehman@gmail.com>
2021-10-27 14:03:33 +0100

    Rewrote model/personalItem.js

--- src/models/personalItem.js
+++ src/models/personalItem.js
@@ -1,18 +1,13 @@
-import { v4 as uuid } from "uuid";
-
-const personalItem = {
-  id: uuid(),
-  item: [
-    { id: "name", name: "Name", type: "text", value: "" },
-    {
-      id: "phone",
-      name: "Phone Number",
-      type: "number",
-      value: "",
-    },
-    { id: "email", name: "Email", type: "email", value: "" },
-    { id: "address", name: "Address", type: "text", value: "" },
-  ],
+const pI = {
+  name: { id: "name", name: "Name", type: "text", value: "" },
+  phone: {
+    id: "phone",
+    name: "Phone Number",
+    type: "number",
+    value: "",
+  },
+  email: { id: "email", name: "Email", type: "email", value: "" },
+  address: { id: "address", name: "Address", type: "text", value: "" },
 };
 
-export default personalItem;
+export default pI;
