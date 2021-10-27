22813b98264a880eab9c4514568d7f3b71488da4
Zia Rehman <iamziarehman@gmail.com>
2021-10-27 14:27:21 +0100

    Imported preview.css to preview.js and added classNames

--- src/preview/preview.js
+++ src/preview/preview.js
@@ -2,27 +2,30 @@ import PersonalItem from "./personalItem";
 import ExperienceItem from "./experienceItem";
 import EducationItem from "./educationItem";
 import SkillsItem from "./skillsItem";
+import "./preview.css";
 
 function Preview({ cv }) {
   return (
     <div className="Preview">
-      <div>
-        <h2>Preview</h2>
+      <div className="preview-container">
         {cv.personal.map((e) => {
           return <PersonalItem id={e.id} element={e.item}></PersonalItem>;
         })}
         <div>
+          <div className="section-header">Experience</div>
           {cv.experience.map((e) => {
             return <ExperienceItem experience={e.item}></ExperienceItem>;
           })}
         </div>
       </div>
       <div>
+        <div className="section-header">Education</div>
         {cv.education.map((e) => {
           return <EducationItem education={e.item}></EducationItem>;
         })}
       </div>
       <div>
+        <div className="section-header">Skills</div>
         {cv.skills.map((e) => {
           const skill = e.item;
           console.log(skill);
