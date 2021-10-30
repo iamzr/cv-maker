b744f03edac6dc295fe969a92e5dc5c33a86c4e9
Zia Rehman <iamziarehman@gmail.com>
2021-10-30 23:39:34 +0100

    Added in textarea input

--- src/form/section.js
+++ src/form/section.js
@@ -19,25 +19,47 @@ function Section(props) {
 
     for (let element in item) {
       const id = item[element]["id"];
-      divs.push(
-        <Form.Group className="mb-3">
-          <Form.Label
-            key={"label-" + itemIndex + id}
-            id={"label-" + itemIndex + id}
-            // htmlFor={id}
-          >
-            {item[element]["name"]}
-          </Form.Label>
-          <Form.Control
-            key={"input-" + itemIndex + id}
-            id={"input-" + itemIndex + id}
-            name={id}
-            type={item[element]["type"]}
-            defaultValue={item[element]["value"]}
-            onChange={(e) => handleChange(e, section, itemIndex, id)}
-          ></Form.Control>
-        </Form.Group>
-      );
+      if (item[element]["type"] === "textarea") {
+        divs.push(
+          <Form.Group className="mb-3">
+            <Form.Label
+              key={"label-" + itemIndex + id}
+              id={"label-" + itemIndex + id}
+              // htmlFor={id}
+            >
+              {item[element]["name"]}
+            </Form.Label>
+            <Form.Control
+              key={"input-" + itemIndex + id}
+              id={"input-" + itemIndex + id}
+              name={id}
+              as={item[element]["type"]}
+              defaultValue={item[element]["value"]}
+              onChange={(e) => handleChange(e, section, itemIndex, id)}
+            ></Form.Control>
+          </Form.Group>
+        );
+      } else {
+        divs.push(
+          <Form.Group className="mb-3">
+            <Form.Label
+              key={"label-" + itemIndex + id}
+              id={"label-" + itemIndex + id}
+              // htmlFor={id}
+            >
+              {item[element]["name"]}
+            </Form.Label>
+            <Form.Control
+              key={"input-" + itemIndex + id}
+              id={"input-" + itemIndex + id}
+              name={id}
+              type={item[element]["type"]}
+              defaultValue={item[element]["value"]}
+              onChange={(e) => handleChange(e, section, itemIndex, id)}
+            ></Form.Control>
+          </Form.Group>
+        );
+      }
     }
     console.log("renderItem done");
     return divs;
